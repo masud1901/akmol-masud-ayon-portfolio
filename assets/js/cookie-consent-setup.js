9afcd39e8c6e0d35320f0593000c2645895b0766
masud1901 <98466537+masud1901@users.noreply.github.com>
2026-04-26 11:31:31 +0000

    Deploying to gh-pages from @ masud1901/akmol-masud-ayon-portfolio@245b0ec2a6af96a749a642878a275942ef446195 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://masud1901.github.io/akmol-masud-ayon-portfolio/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://akmolmasud5.com/#contact">contact us</a>.'
               }
             ]
           }
